f8ca89e9e05cf4c0d587269f18644215846ddd44
programinurdu <aasdb@progd23.com>
2019-02-04 21:49:42 +0000

    Current

---
+++ BasicWebExampleLearn/design/js/Site.js
@@ -0,0 +1,71 @@
+﻿function ShowDate() {
+    //Show DateTime using alert
+    //window.alert(Date());
+    document.getElementById('date').innerHTML = Date();
+}
+
+function ClearDate() {
+    document.getElementById('date').innerHTML = "";
+}
+
+function AddNumbers() {
+
+    var a = parseInt(document.getElementById('first').value);
+    var b = parseInt(document.getElementById('second').value);
+
+    var x = a + b;
+
+    // document.getElementById('demo').innerHtml = "343";
+    //window.alert(x);
+    document.getElementById('result').value = x;
+}
+
+function ClearScreen() {
+    document.getElementById('first').value = "";
+    document.getElementById('second').value = "";
+    document.getElementById('result').value = "";
+
+    document.getElementById('first').focus();
+}
+
+function FindTemp() {
+    var f = parseFloat(document.getElementById('temp').value);
+
+    var c = (5 / 9) * (f - 32);
+
+    document.getElementById('result').value = c;
+}
+
+function SubtractNumbers() {
+    var first = parseInt(document.getElementsByName('first')[0].value);
+    var second = parseInt(document.getElementsByName('second')[0].value);
+
+    var result = first - second;
+
+    document.getElementsByName('result')[0].value = result;
+}
+
+function enabletxt() {
+    //var e = document.getElementById('ddlyesno');
+    //var val = e.options[e.selectedIndex].value;
+
+    var val = document.getElementById('ddlyesno').value;
+
+    if (val === 'yes')
+    {
+        document.getElementById('comment').disabled = false;
+    }
+    else {
+        document.getElementById('comment').disabled = true;
+    }
+}
+
+function enabletxt(val) {
+
+    if (val === 'yes' || val === '') {
+        document.getElementById('comment').disabled = false;
+    }
+    else {
+        document.getElementById('comment').disabled = true;
+    }
+}
